4416faabeec5775a2720da3843cd0eb60870a1aa
Gerred Gruenspan <56643835+ggruenspan@users.noreply.github.com>
2021-02-20 01:44:28 -0500

    Update temps2_home.c

--- Week 4/temps2_home.c
+++ Week 4/temps2_home.c
@@ -1,11 +1,3 @@
-//==============================================
-// Name:           Gerred Gruenspan
-// Student Number: 155315195
-// Email:          ggruenspan@myseneca.ca
-// Section:        NVV
-// Workshop:       4 (in-home)
-//==============================================
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define MAX_NUM 100
