ce14e244656697cc58af6767e95ae255248128ad
Gerred Gruenspan <56643835+ggruenspan@users.noreply.github.com>
2021-02-20 01:54:24 -0500

    Update contactHelpers.c

--- Assingment 2/Mliestone 1 & 2/contactHelpers.c
+++ Assingment 2/Mliestone 1 & 2/contactHelpers.c
@@ -1,14 +1,3 @@
-//==============================================
-// Name:           Gerred Gruenspan
-// Student Number: 155315195
-// Email:          ggruenspan@myseneca.ca
-// Section:        NVV
-// Date:           2020/11/
-//==============================================
-// Assignment:     2
-// Milestone:      1&2
-//==============================================
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
